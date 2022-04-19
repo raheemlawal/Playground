641777579c4b56b19e21465198feab639a56b57d
Raheem Lawal <lawalraheem@gmail.com>
2022-04-19 08:31:18 -0700

    value change

--- java/MapBasics.java
+++ java/MapBasics.java
@@ -5,7 +5,7 @@ public class MapBasics{
         HashMap<String,Integer> hm = new HashMap<String,Integer>();
         
         hm.put("Apple",1);
-        hm.put("Banana",2);
+        hm.put("Banana",3);
         
         for(Map.Entry<String,Integer> me : hm.entrySet()){
             System.out.println(me.getKey());
